c7cc4e8e572dd1851ab4678435c3d38614658460
James <apisitamornktn@gmail.com>
2024-06-28 13:57:32 +0700

    feat:No SQL Creating APIs on Express with MongoDB Assignment

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,51 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("products");
+  const products = await collection.find().limit(10).toArray();
+  return res.json({ data: products });
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  const product = await collection.findOne({ _id: productId });
+  return res.json({ data: product });
+});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
 
-productRouter.put("/:id", (req, res) => {});
+  const productsData = { ...req.body };
+  const products = await collection.insertOne(productsData);
 
-productRouter.delete("/:id", (req, res) => {});
+  return res.json({
+    message: `products record (${products.insertedId}) has been created successfully`,
+  });
+});
+
+productRouter.put("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  const newProductData = { ...req.body };
+
+  await collection.updateOne({ _id: productId }, { $set: newProductData });
+  return res.json({
+    message: `Product record (${productId}) has been updated successfully`,
+  });
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  await collection.deleteOne({ _id: productId });
+  return res.json({
+    message: `Product record (${productId}) has been deleted successfully`,
+  });
+});
 
 export default productRouter;
